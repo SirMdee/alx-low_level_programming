597739294643b49be152812581dbb23d276cf4a4
sirmdee <muhammad1998abdul@gmail.com>
2023-09-25 06:44:56 -0700

    A function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototype for _memset */
+char *_memset(char *s, char b, unsigned int n);
+
+/* Function prototype for simple_print_buffer */
+void simple_print_buffer(char *buffer, unsigned int size);
+
+#endif /* MAIN_H */
